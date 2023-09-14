1d854b3cd8e74cc5a4539646514f3d365feaf9a6
AlenatheFirst <123810723+AlenatheFirst@users.noreply.github.com>
2023-09-14 19:12:29 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  base: '/vite-deploy/',
+  build: {
+    outDir: 'build',
+    sourcemap: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests',
+    mockReset: true,
+  },
+  resolve: {
+    alias: [{find: "@", replacement: path.resolve(__dirname, "src")}]
+  }
+})
